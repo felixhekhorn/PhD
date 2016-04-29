6eda55ae4db03d71363c073c3fe8037e75061f00
Felix Hekhorn <felix.hekhorn@student.uni-tuebingen.de>
2016-04-29 17:44:13 +0200

    fixes for PaVe

--- MMa/data/repG2.m
+++ MMa/data/repG2.m
@@ -6,7 +6,7 @@
  PaVe[3, muR, 0][1][Sqr[k1], Sqr[-k1 + p1], Sqr[p1], 0, 0, m2] -> 
   2/u1 - 2/(u1*\[Epsilon]) - (2*ln[-(u1/m2)])/u1, 
  PaVe[3, muR, 0][2][Sqr[k1], Sqr[-k1 + p1], Sqr[p1], 0, 0, m2] -> 
-  -(ln[-(u1/m2)]/(m2 + u1)), PaVe[3, muR, 0][0, 0][Sqr[k1], Sqr[-k1 + p1], 
+  ln[-(u1/m2)]/(-m2 - u1), PaVe[3, muR, 0][0, 0][Sqr[k1], Sqr[-k1 + p1], 
    Sqr[p1], 0, 0, m2] -> 3/4 - 1/(2*\[Epsilon]) - 
    (u1*ln[-(u1/m2)])/(4*(m2 + u1)), 
  PaVe[3, muR, 0][1, 1][Sqr[k1], Sqr[-k1 + p1], Sqr[p1], 0, 0, m2] -> 
--- MMa/data/repG2cr.m
+++ MMa/data/repG2cr.m
@@ -5,7 +5,7 @@
    ln[-(t1/m2)]^2/t1, PaVe[3, muR, 0][1][Sqr[-k1], Sqr[k1 - p2], Sqr[-p2], 0, 
    0, m2] -> 2/t1 - 2/(t1*\[Epsilon]) - (2*ln[-(t1/m2)])/t1, 
  PaVe[3, muR, 0][2][Sqr[-k1], Sqr[k1 - p2], Sqr[-p2], 0, 0, m2] -> 
-  -(ln[-(t1/m2)]/(m2 + t1)), PaVe[3, muR, 0][0, 0][Sqr[-k1], Sqr[k1 - p2], 
+  ln[-(t1/m2)]/(-m2 - t1), PaVe[3, muR, 0][0, 0][Sqr[-k1], Sqr[k1 - p2], 
    Sqr[-p2], 0, 0, m2] -> 3/4 - 1/(2*\[Epsilon]) - 
    (t1*ln[-(t1/m2)])/(4*(m2 + t1)), 
  PaVe[3, muR, 0][1, 1][Sqr[-k1], Sqr[k1 - p2], Sqr[-p2], 0, 0, m2] -> 
