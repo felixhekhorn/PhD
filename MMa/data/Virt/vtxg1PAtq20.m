f8a5121e30e6e0ab1a2c4c0d2d54400a50dc0751
Felix Hekhorn <felix.hekhorn@student.uni-tuebingen.de>
2016-08-08 17:15:41 +0200

    work on P

---
+++ MMa/data/Virt/vtxg1PAtq20.m
@@ -0,0 +1,30 @@
+(-8*(4*m2^3*t1^3*u1 + 6*m2^3*t1^2*u1^2 + 10*m2^2*t1^3*u1^2 + 4*m2^3*t1*u1^3 + 
+   12*m2^2*t1^2*u1^3 + 8*m2*t1^3*u1^3 + 2*m2^3*u1^4 + 10*m2^2*t1*u1^4 + 
+   6*m2*t1^2*u1^4 + 2*t1^3*u1^4 + 4*m2^2*u1^5 + 8*m2*t1*u1^5 + 2*m2*u1^6 + 
+   2*t1*u1^6 - 2*m2^3*t1^3*u1*\[Epsilon] - 4*m2^3*t1^2*u1^2*\[Epsilon] - 
+   6*m2^2*t1^3*u1^2*\[Epsilon] - 2*m2^3*t1*u1^3*\[Epsilon] - 
+   10*m2^2*t1^2*u1^3*\[Epsilon] - 6*m2*t1^3*u1^3*\[Epsilon] - 
+   5*m2^2*t1*u1^4*\[Epsilon] - 7*m2*t1^2*u1^4*\[Epsilon] - 
+   2*t1^3*u1^4*\[Epsilon] - m2^2*u1^5*\[Epsilon] - 4*m2*t1*u1^5*\[Epsilon] - 
+   t1^2*u1^5*\[Epsilon] - m2*u1^6*\[Epsilon] - t1*u1^6*\[Epsilon] + 
+   2*m2^4*t1^3*zeta2*\[Epsilon] + 4*m2^4*t1^2*u1*zeta2*\[Epsilon] + 
+   4*m2^3*t1^3*u1*zeta2*\[Epsilon] + 2*m2^4*t1*u1^2*zeta2*\[Epsilon] + 
+   8*m2^3*t1^2*u1^2*zeta2*\[Epsilon] + 2*m2^2*t1^3*u1^2*zeta2*\[Epsilon] + 
+   4*m2^3*t1*u1^3*zeta2*\[Epsilon] + 4*m2^2*t1^2*u1^3*zeta2*\[Epsilon] + 
+   2*m2^2*t1*u1^4*zeta2*\[Epsilon] - 2*m2^4*t1^3*\[Epsilon]*
+    Li2[(m2 + u1)/m2] - 4*m2^4*t1^2*u1*\[Epsilon]*Li2[(m2 + u1)/m2] - 
+   4*m2^3*t1^3*u1*\[Epsilon]*Li2[(m2 + u1)/m2] - 2*m2^4*t1*u1^2*\[Epsilon]*
+    Li2[(m2 + u1)/m2] - 8*m2^3*t1^2*u1^2*\[Epsilon]*Li2[(m2 + u1)/m2] - 
+   2*m2^2*t1^3*u1^2*\[Epsilon]*Li2[(m2 + u1)/m2] - 
+   4*m2^3*t1*u1^3*\[Epsilon]*Li2[(m2 + u1)/m2] - 4*m2^2*t1^2*u1^3*\[Epsilon]*
+    Li2[(m2 + u1)/m2] - 2*m2^2*t1*u1^4*\[Epsilon]*Li2[(m2 + u1)/m2] - 
+   10*m2^3*t1^3*u1*\[Epsilon]*ln[-(u1/m2)] - 20*m2^3*t1^2*u1^2*\[Epsilon]*
+    ln[-(u1/m2)] - 17*m2^2*t1^3*u1^2*\[Epsilon]*ln[-(u1/m2)] - 
+   18*m2^3*t1*u1^3*\[Epsilon]*ln[-(u1/m2)] - 26*m2^2*t1^2*u1^3*\[Epsilon]*
+    ln[-(u1/m2)] - 6*m2*t1^3*u1^3*\[Epsilon]*ln[-(u1/m2)] - 
+   8*m2^3*u1^4*\[Epsilon]*ln[-(u1/m2)] - 25*m2^2*t1*u1^4*\[Epsilon]*
+    ln[-(u1/m2)] - 5*m2*t1^2*u1^4*\[Epsilon]*ln[-(u1/m2)] + 
+   t1^3*u1^4*\[Epsilon]*ln[-(u1/m2)] - 8*m2^2*u1^5*\[Epsilon]*ln[-(u1/m2)] - 
+   6*m2*t1*u1^5*\[Epsilon]*ln[-(u1/m2)] + m2*u1^6*\[Epsilon]*ln[-(u1/m2)] + 
+   t1*u1^6*\[Epsilon]*ln[-(u1/m2)]))/(u1^3*(m2 + u1)^2*(t1^2 + t1*u1)*
+  \[Epsilon])
---
+++ MMa/data/Virt/vtxg2PAtq20.m
@@ -0,0 +1,61 @@
+(2*(24*m2^3*t1^3 + 40*m2^3*t1^2*u1 + 56*m2^2*t1^3*u1 + 24*m2^3*t1*u1^2 + 
+   80*m2^2*t1^2*u1^2 + 40*m2*t1^3*u1^2 + 8*m2^3*u1^3 + 56*m2^2*t1*u1^3 + 
+   40*m2*t1^2*u1^3 + 8*t1^3*u1^3 + 16*m2^2*u1^4 + 40*m2*t1*u1^4 + 8*m2*u1^5 + 
+   8*t1*u1^5 + 24*m2^3*t1^3*\[Epsilon] + 40*m2^3*t1^2*u1*\[Epsilon] + 
+   56*m2^2*t1^3*u1*\[Epsilon] + 24*m2^3*t1*u1^2*\[Epsilon] + 
+   80*m2^2*t1^2*u1^2*\[Epsilon] + 40*m2*t1^3*u1^2*\[Epsilon] + 
+   8*m2^3*u1^3*\[Epsilon] + 56*m2^2*t1*u1^3*\[Epsilon] + 
+   40*m2*t1^2*u1^3*\[Epsilon] + 8*t1^3*u1^3*\[Epsilon] + 
+   16*m2^2*u1^4*\[Epsilon] + 40*m2*t1*u1^4*\[Epsilon] + 
+   8*m2*u1^5*\[Epsilon] + 8*t1*u1^5*\[Epsilon] - 8*m2^3*t1^3*\[Epsilon]^2 - 
+   16*m2^3*t1^2*u1*\[Epsilon]^2 - 16*m2^2*t1^3*u1*\[Epsilon]^2 - 
+   8*m2^3*t1*u1^2*\[Epsilon]^2 - 32*m2^2*t1^2*u1^2*\[Epsilon]^2 - 
+   8*m2*t1^3*u1^2*\[Epsilon]^2 - 20*m2^2*t1*u1^3*\[Epsilon]^2 - 
+   12*m2*t1^2*u1^3*\[Epsilon]^2 - 4*m2^2*u1^4*\[Epsilon]^2 - 
+   16*m2*t1*u1^4*\[Epsilon]^2 + 4*t1^2*u1^4*\[Epsilon]^2 - 
+   4*m2*u1^5*\[Epsilon]^2 - 4*t1*u1^5*\[Epsilon]^2 + 
+   3*m2^3*t1^3*zeta2*\[Epsilon]^2 + 5*m2^3*t1^2*u1*zeta2*\[Epsilon]^2 + 
+   7*m2^2*t1^3*u1*zeta2*\[Epsilon]^2 + 3*m2^3*t1*u1^2*zeta2*\[Epsilon]^2 + 
+   10*m2^2*t1^2*u1^2*zeta2*\[Epsilon]^2 + 5*m2*t1^3*u1^2*zeta2*\[Epsilon]^2 + 
+   m2^3*u1^3*zeta2*\[Epsilon]^2 + 7*m2^2*t1*u1^3*zeta2*\[Epsilon]^2 + 
+   5*m2*t1^2*u1^3*zeta2*\[Epsilon]^2 + t1^3*u1^3*zeta2*\[Epsilon]^2 + 
+   2*m2^2*u1^4*zeta2*\[Epsilon]^2 + 5*m2*t1*u1^4*zeta2*\[Epsilon]^2 + 
+   m2*u1^5*zeta2*\[Epsilon]^2 + t1*u1^5*zeta2*\[Epsilon]^2 + 
+   12*m2^3*t1^3*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 20*m2^3*t1^2*u1*\[Epsilon]^2*
+    Li2[(m2 + u1)/m2] + 28*m2^2*t1^3*u1*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 
+   12*m2^3*t1*u1^2*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 
+   40*m2^2*t1^2*u1^2*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 
+   20*m2*t1^3*u1^2*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 
+   4*m2^3*u1^3*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 28*m2^2*t1*u1^3*\[Epsilon]^2*
+    Li2[(m2 + u1)/m2] + 20*m2*t1^2*u1^3*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 
+   4*t1^3*u1^3*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 8*m2^2*u1^4*\[Epsilon]^2*
+    Li2[(m2 + u1)/m2] + 20*m2*t1*u1^4*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 
+   4*m2*u1^5*\[Epsilon]^2*Li2[(m2 + u1)/m2] + 4*t1*u1^5*\[Epsilon]^2*
+    Li2[(m2 + u1)/m2] + 24*m2^3*t1^3*\[Epsilon]*ln[-(u1/m2)] + 
+   40*m2^3*t1^2*u1*\[Epsilon]*ln[-(u1/m2)] + 56*m2^2*t1^3*u1*\[Epsilon]*
+    ln[-(u1/m2)] + 24*m2^3*t1*u1^2*\[Epsilon]*ln[-(u1/m2)] + 
+   80*m2^2*t1^2*u1^2*\[Epsilon]*ln[-(u1/m2)] + 40*m2*t1^3*u1^2*\[Epsilon]*
+    ln[-(u1/m2)] + 8*m2^3*u1^3*\[Epsilon]*ln[-(u1/m2)] + 
+   56*m2^2*t1*u1^3*\[Epsilon]*ln[-(u1/m2)] + 40*m2*t1^2*u1^3*\[Epsilon]*
+    ln[-(u1/m2)] + 8*t1^3*u1^3*\[Epsilon]*ln[-(u1/m2)] + 
+   16*m2^2*u1^4*\[Epsilon]*ln[-(u1/m2)] + 40*m2*t1*u1^4*\[Epsilon]*
+    ln[-(u1/m2)] + 8*m2*u1^5*\[Epsilon]*ln[-(u1/m2)] + 
+   8*t1*u1^5*\[Epsilon]*ln[-(u1/m2)] - 40*m2^3*t1^3*\[Epsilon]^2*
+    ln[-(u1/m2)] - 88*m2^3*t1^2*u1*\[Epsilon]^2*ln[-(u1/m2)] - 
+   72*m2^2*t1^3*u1*\[Epsilon]^2*ln[-(u1/m2)] - 88*m2^3*t1*u1^2*\[Epsilon]^2*
+    ln[-(u1/m2)] - 116*m2^2*t1^2*u1^2*\[Epsilon]^2*ln[-(u1/m2)] - 
+   32*m2*t1^3*u1^2*\[Epsilon]^2*ln[-(u1/m2)] - 40*m2^3*u1^3*\[Epsilon]^2*
+    ln[-(u1/m2)] - 120*m2^2*t1*u1^3*\[Epsilon]^2*ln[-(u1/m2)] - 
+   24*m2*t1^2*u1^3*\[Epsilon]^2*ln[-(u1/m2)] - 44*m2^2*u1^4*\[Epsilon]^2*
+    ln[-(u1/m2)] - 32*m2*t1*u1^4*\[Epsilon]^2*ln[-(u1/m2)] + 
+   12*m2^3*t1^3*\[Epsilon]^2*ln[-(u1/m2)]^2 + 20*m2^3*t1^2*u1*\[Epsilon]^2*
+    ln[-(u1/m2)]^2 + 28*m2^2*t1^3*u1*\[Epsilon]^2*ln[-(u1/m2)]^2 + 
+   12*m2^3*t1*u1^2*\[Epsilon]^2*ln[-(u1/m2)]^2 + 
+   40*m2^2*t1^2*u1^2*\[Epsilon]^2*ln[-(u1/m2)]^2 + 
+   20*m2*t1^3*u1^2*\[Epsilon]^2*ln[-(u1/m2)]^2 + 
+   4*m2^3*u1^3*\[Epsilon]^2*ln[-(u1/m2)]^2 + 28*m2^2*t1*u1^3*\[Epsilon]^2*
+    ln[-(u1/m2)]^2 + 20*m2*t1^2*u1^3*\[Epsilon]^2*ln[-(u1/m2)]^2 + 
+   4*t1^3*u1^3*\[Epsilon]^2*ln[-(u1/m2)]^2 + 8*m2^2*u1^4*\[Epsilon]^2*
+    ln[-(u1/m2)]^2 + 20*m2*t1*u1^4*\[Epsilon]^2*ln[-(u1/m2)]^2 + 
+   4*m2*u1^5*\[Epsilon]^2*ln[-(u1/m2)]^2 + 4*t1*u1^5*\[Epsilon]^2*
+    ln[-(u1/m2)]^2))/(t1*u1^2*(m2 + u1)^2*(t1 + u1)*\[Epsilon]^2)
