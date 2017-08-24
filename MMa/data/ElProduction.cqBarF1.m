594356ef8b7804b03b4e1d271a02eb9abc10788e
Felix Hekhorn <felix.hekhorn@student.uni-tuebingen.de>
2017-08-24 18:29:21 +0200

    work on cgBar1@HE, cqBarF1

---
+++ MMa/data/ElProduction.cqBarF1.m
@@ -0,0 +1,133 @@
+(* Created by Wolfram Mathematica 8.0 : www.wolfram.com *)
+{
+ -(Pi*\[Chi]*\[Chi]q*(\[Chi]q + 
+      \[Chi]*(2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q))))/
+   (9*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) - 
+  (2*(-1 + \[Chi])*(1 + \[Chi])*\[Chi]q^2*
+    (2*\[Chi]q^2*(1 + \[Chi]q*(5 + \[Chi]q)) + 2*\[Chi]^4*\[Chi]q^2*
+      (1 + \[Chi]q*(5 + \[Chi]q)) + \[Chi]*\[Chi]q*
+      (7 + \[Chi]q*(20 + \[Chi]q*(2 + \[Chi]q*(20 + 7*\[Chi]q)))) + 
+     \[Chi]^3*\[Chi]q*(7 + \[Chi]q*(20 + \[Chi]q*
+          (2 + \[Chi]q*(20 + 7*\[Chi]q)))) + 
+     \[Chi]^2*(7 + \[Chi]q*(2 + \[Chi]q*(5 + \[Chi]q*
+            (56 + \[Chi]q*(5 + \[Chi]q*(2 + 7*\[Chi]q))))))))/
+   (9*Pi*(\[Chi] + \[Chi]q)^3*(1 + \[Chi]*\[Chi]q)^3*(-1 + \[Chi]q^2)^2) - 
+  (4*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q)))*Li2[-\[Chi]])/
+   (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) + 
+  (2*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q)))*
+    Li2[(1 - \[Chi]q)/(1 + \[Chi])])/(3*Pi*(\[Chi] + \[Chi]q)^2*
+    (1 + \[Chi]*\[Chi]q)^2) + 
+  (2*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q)))*
+    Li2[(-1 + \[Chi]q)/((1 + \[Chi])*\[Chi]q)])/(3*Pi*(\[Chi] + \[Chi]q)^2*
+    (1 + \[Chi]*\[Chi]q)^2) - 
+  (2*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q)))*
+    Li2[(\[Chi]*(-1 + \[Chi]q))/((1 + \[Chi])*\[Chi]q)])/
+   (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) - 
+  (2*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q)))*
+    Li2[(\[Chi] - \[Chi]*\[Chi]q)/(1 + \[Chi])])/(3*Pi*(\[Chi] + \[Chi]q)^2*
+    (1 + \[Chi]*\[Chi]q)^2) + 
+  (\[Chi]*\[Chi]q*(\[Chi]q + \[Chi]*(2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q)))*
+    ln[\[Chi]]^2)/(3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) + 
+  (4*\[Chi]q*(-(\[Chi]q*(1 + \[Chi]q^2)*(1 + \[Chi]q*(5 + \[Chi]q))) - 
+     \[Chi]^2*\[Chi]q*(1 + \[Chi]q^2)*(1 + \[Chi]q*(5 + \[Chi]q)) - 
+     \[Chi]*(1 + \[Chi]q*(-4 + \[Chi]q*(3 + \[Chi]q*
+            (28 + (-3 + \[Chi]q)*(-1 + \[Chi]q)*\[Chi]q)))))*
+    ln[\[Chi] + \[Chi]q])/(9*Pi*(\[Chi] + \[Chi]q)*(1 + \[Chi]*\[Chi]q)*
+    (-1 + \[Chi]q^2)^3) + 
+  (4*\[Chi]q*(\[Chi]q*(1 + \[Chi]q^2)*(1 + \[Chi]q*(5 + \[Chi]q)) + 
+     \[Chi]^2*\[Chi]q*(1 + \[Chi]q^2)*(1 + \[Chi]q*(5 + \[Chi]q)) + 
+     \[Chi]*(1 + \[Chi]q*(-4 + \[Chi]q*(3 + \[Chi]q*
+            (28 + (-3 + \[Chi]q)*(-1 + \[Chi]q)*\[Chi]q)))))*
+    ln[1 + \[Chi]*\[Chi]q])/(9*Pi*(\[Chi] + \[Chi]q)*(1 + \[Chi]*\[Chi]q)*
+    (-1 + \[Chi]q^2)^3) + ln[\[Chi]]*
+   ((\[Chi]*\[Chi]q*(3*\[Chi]q^2 + 3*\[Chi]^4*\[Chi]q^2 + 
+       3*\[Chi]*\[Chi]q*(1 + \[Chi]q)^2 + 3*\[Chi]^3*\[Chi]q*
+        (1 + \[Chi]q)^2 - 2*\[Chi]^2*(2 + \[Chi]q*(-11 + \[Chi]q - 
+           11*\[Chi]q^2 + 2*\[Chi]q^3))))/(9*Pi*(\[Chi] + \[Chi]q)^3*
+      (1 + \[Chi]*\[Chi]q)^3) + 
+    (2*\[Chi]*\[Chi]q*(\[Chi]q + \[Chi]*
+        (2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q)))*ln[\[Chi]q])/
+     (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) - 
+    (2*\[Chi]*\[Chi]q*(\[Chi]q + \[Chi]*
+        (2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q)))*ln[\[Chi] + \[Chi]q])/
+     (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) - 
+    (2*\[Chi]*\[Chi]q*(\[Chi]q + \[Chi]*
+        (2 + \[Chi]q*(-4 + \[Chi] + 2*\[Chi]q)))*ln[1 + \[Chi]*\[Chi]q])/
+     (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2)), 
+ (4*(-1 + \[Chi])*(1 + \[Chi])*\[Chi]q^2*(-3*\[Chi]^2 + 8*\[Chi]^2*\[Chi]q + 
+     (1 + \[Chi] + \[Chi]^2)*(1 + \[Chi]*(7 + \[Chi]))*\[Chi]q^2 + 
+     4*(1 + \[Chi])^2*(1 + \[Chi]^2)*\[Chi]q^3 + (1 + \[Chi] + \[Chi]^2)*
+      (1 + \[Chi]*(7 + \[Chi]))*\[Chi]q^4 + 8*\[Chi]^2*\[Chi]q^5 - 
+     3*\[Chi]^2*\[Chi]q^6))/(9*Pi*(\[Chi] + \[Chi]q)^3*(1 + \[Chi]*\[Chi]q)^3*
+    (-1 + \[Chi]q^2)^2) + (4*\[Chi]^2*(-1 + \[Chi]q)^2*\[Chi]q*
+    (3*\[Chi]q + \[Chi]*(3 + \[Chi]q*(4 + 3*\[Chi] + 3*\[Chi]q)))*ln[\[Chi]])/
+   (9*Pi*(\[Chi] + \[Chi]q)^3*(1 + \[Chi]*\[Chi]q)^3) + 
+  (4*\[Chi]q*(4*\[Chi]q^3*(1 + \[Chi]q + \[Chi]q^2) + 
+     4*\[Chi]^4*\[Chi]q^3*(1 + \[Chi]q + \[Chi]q^2) + 
+     2*\[Chi]*\[Chi]q^2*(1 + \[Chi]q*
+        (4 + \[Chi]q*(14 + \[Chi]q*(4 + \[Chi]q)))) + 
+     2*\[Chi]^3*\[Chi]q^2*
+      (1 + \[Chi]q*(4 + \[Chi]q*(14 + \[Chi]q*(4 + \[Chi]q)))) + 
+     \[Chi]^2*(3 + \[Chi]q*(-2 + \[Chi]q*(-8 + \[Chi]q*
+            (26 + \[Chi]q*(34 + \[Chi]q*(26 + (-2 + \[Chi]q)*\[Chi]q*
+                  (4 + 3*\[Chi]q))))))))*ln[\[Chi] + \[Chi]q])/
+   (9*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2*(-1 + \[Chi]q^2)^3) + 
+  (4*\[Chi]q*(-4*\[Chi]q^3*(1 + \[Chi]q + \[Chi]q^2) - 
+     4*\[Chi]^4*\[Chi]q^3*(1 + \[Chi]q + \[Chi]q^2) - 
+     2*\[Chi]*\[Chi]q^2*(1 + \[Chi]q*
+        (4 + \[Chi]q*(14 + \[Chi]q*(4 + \[Chi]q)))) - 
+     2*\[Chi]^3*\[Chi]q^2*
+      (1 + \[Chi]q*(4 + \[Chi]q*(14 + \[Chi]q*(4 + \[Chi]q)))) + 
+     \[Chi]^2*(-3 + \[Chi]q*(2 + \[Chi]q*
+          (8 - \[Chi]q*(26 + \[Chi]q*(34 + \[Chi]q*(26 + (-2 + \[Chi]q)*
+                  \[Chi]q*(4 + 3*\[Chi]q))))))))*ln[1 + \[Chi]*\[Chi]q])/
+   (9*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2*(-1 + \[Chi]q^2)^3), 
+ (-4*\[Chi]*(-1 + \[Chi]^2)*\[Chi]q^2)/(Pi*(\[Chi] + \[Chi]q)^2*
+    (1 + \[Chi]*\[Chi]q)^2) - 
+  (Pi*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q))))/
+   (9*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) - 
+  (4*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q)))*Li2[-\[Chi]])/
+   (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) + 
+  (2*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q)))*
+    Li2[(1 - \[Chi]q)/(1 + \[Chi])])/(3*Pi*(\[Chi] + \[Chi]q)^2*
+    (1 + \[Chi]*\[Chi]q)^2) + 
+  (2*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q)))*
+    Li2[(-1 + \[Chi]q)/((1 + \[Chi])*\[Chi]q)])/(3*Pi*(\[Chi] + \[Chi]q)^2*
+    (1 + \[Chi]*\[Chi]q)^2) - 
+  (2*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q)))*
+    Li2[(\[Chi]*(-1 + \[Chi]q))/((1 + \[Chi])*\[Chi]q)])/
+   (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) - 
+  (2*\[Chi]*\[Chi]q*(\[Chi]q + 
+     \[Chi]*(2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q)))*
+    Li2[(\[Chi] - \[Chi]*\[Chi]q)/(1 + \[Chi])])/(3*Pi*(\[Chi] + \[Chi]q)^2*
+    (1 + \[Chi]*\[Chi]q)^2) + 
+  (\[Chi]*\[Chi]q*(\[Chi]q + \[Chi]*(2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q)))*
+    ln[\[Chi]]^2)/(3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) + 
+  (2*\[Chi]*\[Chi]q*(1 + \[Chi]q)*(\[Chi]q + 
+     \[Chi]*(3 + \[Chi]q*(-4 + \[Chi] + 3*\[Chi]q)))*ln[\[Chi] + \[Chi]q])/
+   (3*Pi*(-1 + \[Chi]q)*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) - 
+  (2*\[Chi]*\[Chi]q*(1 + \[Chi]q)*(\[Chi]q + 
+     \[Chi]*(3 + \[Chi]q*(-4 + \[Chi] + 3*\[Chi]q)))*ln[1 + \[Chi]*\[Chi]q])/
+   (3*Pi*(-1 + \[Chi]q)*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) + 
+  ln[\[Chi]]*((\[Chi]*\[Chi]q*(7*\[Chi]q + 
+       \[Chi]*(6 + \[Chi]q*(6 + 7*\[Chi] + 6*\[Chi]q))))/
+     (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) + 
+    (2*\[Chi]*\[Chi]q*(\[Chi]q + \[Chi]*
+        (2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q)))*ln[\[Chi]q])/
+     (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) - 
+    (2*\[Chi]*\[Chi]q*(\[Chi]q + \[Chi]*
+        (2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q)))*ln[\[Chi] + \[Chi]q])/
+     (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2) - 
+    (2*\[Chi]*\[Chi]q*(\[Chi]q + \[Chi]*
+        (2 + \[Chi]q*(-2 + \[Chi] + 2*\[Chi]q)))*ln[1 + \[Chi]*\[Chi]q])/
+     (3*Pi*(\[Chi] + \[Chi]q)^2*(1 + \[Chi]*\[Chi]q)^2))}
