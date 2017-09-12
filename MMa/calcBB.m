17ac6cb68220afb486cc500ffa6ba897a72fb1e9
Felix Hekhorn <felix.hekhorn@student.uni-tuebingen.de>
2017-09-12 17:38:24 +0200

    remove old backups, work on BB

--- MMa/calcBB.m
+++ MMa/calcBB.m
@@ -12,25 +12,30 @@ OnShell[on,{k1,k1,0},{q,q,q2},{p1,p1,m2},{p2,p2,m2},
            {k1,p2,-t1/2},{k1,p1,-u1/2},
            {q,p1,-(t1-q2)/2},{q,p2,-(u1-q2)/2}];
 
-proj[ph][GG][mu_, mup_] := -({mu}.{mup});
-proj[ph][L][mu_, mup_] := -4 q2/sp^2 S[k1,{mu}] S[k1,{mup}];
-proj[ph][P][mu_, mup_] := I Eps[{mu}, {mup}, q, k1] /sp
-proj[ph][A][mu_, mup_] := 0
+proj[V][FG][mu_, mup_] := -({mu}.{mup});
+proj[V][FL][mu_, mup_] := -4 q2/sp^2 S[k1,{mu}] S[k1,{mup}];
+proj[V][x2g1][mu_, mup_] := I Eps[{mu}, {mup}, q, k1] /sp
+proj[V][xF3][mu_, mup_] := proj[V][x2g1][mu, mup]
+proj[V][gG][mu_, mup_] := -proj[V][FG][mu, mup]
+proj[V][gL][mu_, mup_] := -proj[V][FL][mu, mup]
 
-proj[g][GG][nu_, nup_] := -({nu}.{nup});
-proj[g][L][nu_, nup_] := proj[g][GG][nu, nup]
-proj[g][P][nu_, nup_] := 2 I Eps[{nu}, {nup}, k1, q] /sp;
+proj[g][FG][nu_, nup_]   := -({nu}.{nup});
+proj[g][FL][nu_, nup_]   := proj[g][FG][nu, nup]
+proj[g][xF3][nu_, nup_]  := proj[g][FG][nu, nup]
+proj[g][x2g1][nu_, nup_] := 2 I Eps[{nu}, {nup}, k1, q] /sp;
+proj[g][gG][nu_, nup_]   := proj[g][x2g1][nu, nup]
+proj[g][gL][nu_, nup_]   := proj[g][x2g1][nu, nup]
 
 Gint[V][mu_] := {{mu}};
 Gint[A][mu_] := {(U+G5),{mu},(U-G5)};
 
 (* matrix elements *)
-meLO[t1][Gint_][mu_, nu_] :=  Gint[mu]~Join~{(k1-p2 + Sqrt@m2 U), {nu}}
-meLO[u1][Gint_][mu_, nu_] :=  {{nu},(p1-k1 + Sqrt@m2 U)}~Join~Gint[mu];
+meLO[t1][Gint_][mu_, nu_] :=  Gint[mu] ~Join~ {(k1-p2 + Sqrt@m2 U), {nu}}
+meLO[u1][Gint_][mu_, nu_] :=  {{nu},(p1-k1 + Sqrt@m2 U)} ~Join~ Gint[mu];
 
 (* calculate *)
 applyProj[tr_,k_] := Module[{f},
-f = ContractEpsGamma[proj[ph][k][mu,mup]*tr];
+f = ContractEpsGamma[proj[V][k][mu,mup]*tr];
 f = ContractEpsGamma[proj[g][k][nu,nup]*f];
 (*f = f/.{H[a_,b_]:>h[a,b]}/.{h[a_+b_,c_]:>h[a,c]+h[b,c],h[a_,c_+d_]:>h[a,c]+h[a,d]}/.{h[a_,b_]:>H[a,b]}
 f = RemoveHatMomenta[f,k1,q,p1,p2];*)
@@ -46,9 +51,12 @@ Print@StringJoin["elems[",ToString@First@ind,"] = ",ToString@e];
 line = {(p1 + Sqrt@m2 U)} ~Join~ meLO[ch1][Gint[cur1]][mu, nu] ~Join~ {(p2 - Sqrt@m2 U)} ~Join~ Reverse[meLO[ch2][Gint[cur2]][mup, nup]];
 PrependTo[line,l@@e];
 tr = GammaTrace@@line;
-(BB@@e)@GG = applyProj[tr,GG];
-(BB@@e)@L = applyProj[tr,L];
-(BB@@e)@P = applyProj[tr,P];
+(BB@@e)@FG   = applyProj[tr,FG];
+(BB@@e)@FL   = applyProj[tr,FL];
+(BB@@e)@xF3  = applyProj[tr,xF3];
+(BB@@e)@x2g1 = applyProj[tr,x2g1];
+(BB@@e)@gG   = applyProj[tr,gG];
+(BB@@e)@gL   = applyProj[tr,gL];
 ]
 
 MapIndexed[calcTr, elems];
--- MMa/data/BB.m
+++ MMa/data/BB.m
@@ -1,143 +1,283 @@
-BB[A, A, t1, t1][GG] = -512*m2^2 - 256*m2*t1
+BB[A, A, t1, t1][FG] = -512*m2^2 - 256*m2*t1
  
-BB[A, A, t1, t1][L] = 0
+BB[A, A, t1, t1][FL] = 0
  
-BB[A, A, t1, t1][P] = 0
+BB[A, A, t1, t1][gG] = 0
  
-BB[A, A, t1, u1][GG] = -512*m2^2 + 256*m2*s + 128*m2*t1 + 128*m2*u1 - 
+BB[A, A, t1, t1][gL] = 0
+ 
+BB[A, A, t1, t1][x2g1] = 0
+ 
+BB[A, A, t1, t1][xF3] = 0
+ 
+BB[A, A, t1, u1][FG] = -512*m2^2 + 256*m2*s + 128*m2*t1 + 128*m2*u1 - 
      128*t1*u1 + 32*n*t1*u1
  
-BB[A, A, t1, u1][L] = 0
+BB[A, A, t1, u1][FL] = 0
  
-BB[A, A, t1, u1][P] = 0
+BB[A, A, t1, u1][gG] = ((-1024*I)*epsi*m2)/sp
  
-BB[A, A, u1, t1][GG] = -512*m2^2 + 256*m2*s + 128*m2*t1 + 128*m2*u1 - 
+BB[A, A, t1, u1][gL] = 0
+ 
+BB[A, A, t1, u1][x2g1] = 0
+ 
+BB[A, A, t1, u1][xF3] = -32*m2*t1 - 32*m2*u1 + 64*t1*u1 - 16*n*t1*u1
+ 
+BB[A, A, u1, t1][FG] = -512*m2^2 + 256*m2*s + 128*m2*t1 + 128*m2*u1 - 
      128*t1*u1 + 32*n*t1*u1
  
-BB[A, A, u1, t1][L] = 0
+BB[A, A, u1, t1][FL] = 0
  
-BB[A, A, u1, t1][P] = 0
+BB[A, A, u1, t1][gG] = ((1024*I)*epsi*m2)/sp
  
-BB[A, A, u1, u1][GG] = -512*m2^2 - 256*m2*u1
+BB[A, A, u1, t1][gL] = 0
  
-BB[A, A, u1, u1][L] = 0
+BB[A, A, u1, t1][x2g1] = 0
  
-BB[A, A, u1, u1][P] = 0
+BB[A, A, u1, t1][xF3] = 32*m2*t1 + 32*m2*u1 - 64*t1*u1 + 16*n*t1*u1
  
-BB[A, V, t1, t1][GG] = -64*m2^2 - 32*m2*s - 64*m2*t1 - 32*m2*u1 - 16*t1*u1 + 
+BB[A, A, u1, u1][FG] = -512*m2^2 - 256*m2*u1
+ 
+BB[A, A, u1, u1][FL] = 0
+ 
+BB[A, A, u1, u1][gG] = 0
+ 
+BB[A, A, u1, u1][gL] = 0
+ 
+BB[A, A, u1, u1][x2g1] = 0
+ 
+BB[A, A, u1, u1][xF3] = 0
+ 
+BB[A, V, t1, t1][FG] = -64*m2^2 - 32*m2*s - 64*m2*t1 - 32*m2*u1 - 16*t1*u1 + 
      8*n*t1*u1
  
-BB[A, V, t1, t1][L] = (64*m2*q2*t1*u1)/sp^2
+BB[A, V, t1, t1][FL] = (64*m2*q2*t1*u1)/sp^2
  
-BB[A, V, t1, t1][P] = -16*m2*u1 + 8*t1*u1 - (16*q2*t1*u1)/sp - 
+BB[A, V, t1, t1][gG] = -32*m2*t1 + 16*s*t1 + (16*q2*t1^2)/sp - (16*t1^3)/sp - 
+     32*m2*u1 + 16*t1*u1 - (16*q2*t1*u1)/sp + (16*t1*u1^2)/sp
+ 
+BB[A, V, t1, t1][gL] = 0
+ 
+BB[A, V, t1, t1][x2g1] = -16*m2*u1 + 8*t1*u1 - (16*q2*t1*u1)/sp - 
      (16*q2*t1^2*u1)/sp^2 + (16*t1*u1^2)/sp
  
-BB[A, V, t1, u1][GG] = -64*m2^2 + 16*s^2 - 16*m2*t1 + 16*s*t1 - 16*m2*u1 + 
+BB[A, V, t1, t1][xF3] = (16*m2*q2*t1)/sp - (16*m2*t1^2)/sp + 16*m2*u1 - 
+     (16*m2*q2*u1)/sp + 8*t1*u1 - 4*n*t1*u1 + (16*m2*u1^2)/sp
+ 
+BB[A, V, t1, u1][FG] = -64*m2^2 + 16*s^2 - 16*m2*t1 + 16*s*t1 - 16*m2*u1 + 
      16*s*u1 - 32*t1*u1 + 8*n*t1*u1
  
-BB[A, V, t1, u1][L] = (64*m2*q2*t1*u1)/sp^2 - (32*q2*s*t1*u1)/sp^2
+BB[A, V, t1, u1][FL] = (64*m2*q2*t1*u1)/sp^2 - (32*q2*s*t1*u1)/sp^2
  
-BB[A, V, t1, u1][P] = -8*m2*t1 + 4*s*t1 - ((32*I)*epsi*q2*t1)/sp^2 + 
+BB[A, V, t1, u1][gG] = ((-128*I)*epsi*m2)/sp - ((64*I)*epsi*s)/sp - 8*s*t1 - 
+     ((32*I)*epsi*t1)/sp - (8*q2*t1^2)/sp + (8*t1^3)/sp + 8*s*u1 - 
+     ((32*I)*epsi*u1)/sp + (8*t1^2*u1)/sp + (8*q2*u1^2)/sp - (8*t1*u1^2)/sp - 
+     (8*u1^3)/sp
+ 
+BB[A, V, t1, u1][gL] = ((128*I)*epsi*q2*t1*u1)/sp^3
+ 
+BB[A, V, t1, u1][x2g1] = -8*m2*t1 + 4*s*t1 - ((32*I)*epsi*q2*t1)/sp^2 + 
      ((16*I)*epsi*t1)/sp + ((32*I)*epsi*t1^2)/sp^2 + (4*q2*t1^2)/sp - 
      (4*t1^3)/sp - 8*m2*u1 + 4*s*u1 + ((32*I)*epsi*q2*u1)/sp^2 - 
      ((16*I)*epsi*u1)/sp + (8*q2*t1*u1)/sp + (8*q2*t1^2*u1)/sp^2 - 
      (4*t1^2*u1)/sp - ((32*I)*epsi*u1^2)/sp^2 + (4*q2*u1^2)/sp + 
      (8*q2*t1*u1^2)/sp^2 - (4*t1*u1^2)/sp - (4*u1^3)/sp
  
-BB[A, V, u1, t1][GG] = -64*m2^2 + 16*s^2 - 16*m2*t1 + 16*s*t1 - 16*m2*u1 + 
+BB[A, V, t1, u1][xF3] = -16*m2*t1 + 4*s*t1 + ((16*I)*epsi*t1)/sp + 
+     (16*m2*q2*t1)/sp - (8*q2*s*t1)/sp - (16*m2*t1^2)/sp - (4*q2*t1^2)/sp + 
+     (8*s*t1^2)/sp + (4*t1^3)/sp - 4*s*u1 + ((16*I)*epsi*u1)/sp - 
+     (16*m2*q2*u1)/sp + (8*q2*s*u1)/sp + 16*t1*u1 - 4*n*t1*u1 + 
+     (4*t1^2*u1)/sp + (16*m2*u1^2)/sp + (4*q2*u1^2)/sp - (8*s*u1^2)/sp - 
+     (4*t1*u1^2)/sp - (4*u1^3)/sp
+ 
+BB[A, V, u1, t1][FG] = -64*m2^2 + 16*s^2 - 16*m2*t1 + 16*s*t1 - 16*m2*u1 + 
      16*s*u1 - 32*t1*u1 + 8*n*t1*u1
  
-BB[A, V, u1, t1][L] = (64*m2*q2*t1*u1)/sp^2 - (32*q2*s*t1*u1)/sp^2
+BB[A, V, u1, t1][FL] = (64*m2*q2*t1*u1)/sp^2 - (32*q2*s*t1*u1)/sp^2
  
-BB[A, V, u1, t1][P] = -8*m2*t1 + 4*s*t1 + ((32*I)*epsi*q2*t1)/sp^2 - 
+BB[A, V, u1, t1][gG] = ((128*I)*epsi*m2)/sp + ((64*I)*epsi*s)/sp - 8*s*t1 + 
+     ((32*I)*epsi*t1)/sp - (8*q2*t1^2)/sp + (8*t1^3)/sp + 8*s*u1 + 
+     ((32*I)*epsi*u1)/sp + (8*t1^2*u1)/sp + (8*q2*u1^2)/sp - (8*t1*u1^2)/sp - 
+     (8*u1^3)/sp
+ 
+BB[A, V, u1, t1][gL] = ((-128*I)*epsi*q2*t1*u1)/sp^3
+ 
+BB[A, V, u1, t1][x2g1] = -8*m2*t1 + 4*s*t1 + ((32*I)*epsi*q2*t1)/sp^2 - 
      ((16*I)*epsi*t1)/sp - ((32*I)*epsi*t1^2)/sp^2 + (4*q2*t1^2)/sp - 
      (4*t1^3)/sp - 8*m2*u1 + 4*s*u1 - ((32*I)*epsi*q2*u1)/sp^2 + 
      ((16*I)*epsi*u1)/sp + (8*q2*t1*u1)/sp + (8*q2*t1^2*u1)/sp^2 - 
      (4*t1^2*u1)/sp + ((32*I)*epsi*u1^2)/sp^2 + (4*q2*u1^2)/sp + 
      (8*q2*t1*u1^2)/sp^2 - (4*t1*u1^2)/sp - (4*u1^3)/sp
  
-BB[A, V, u1, u1][GG] = -64*m2^2 - 32*m2*s - 32*m2*t1 - 64*m2*u1 - 16*t1*u1 + 
+BB[A, V, u1, t1][xF3] = 4*s*t1 - ((16*I)*epsi*t1)/sp + (16*m2*q2*t1)/sp - 
+     (8*q2*s*t1)/sp - (16*m2*t1^2)/sp - (4*q2*t1^2)/sp + (8*s*t1^2)/sp + 
+     (4*t1^3)/sp + 16*m2*u1 - 4*s*u1 - ((16*I)*epsi*u1)/sp - 
+     (16*m2*q2*u1)/sp + (8*q2*s*u1)/sp - 16*t1*u1 + 4*n*t1*u1 + 
+     (4*t1^2*u1)/sp + (16*m2*u1^2)/sp + (4*q2*u1^2)/sp - (8*s*u1^2)/sp - 
+     (4*t1*u1^2)/sp - (4*u1^3)/sp
+ 
+BB[A, V, u1, u1][FG] = -64*m2^2 - 32*m2*s - 32*m2*t1 - 64*m2*u1 - 16*t1*u1 + 
      8*n*t1*u1
  
-BB[A, V, u1, u1][L] = (64*m2*q2*t1*u1)/sp^2
+BB[A, V, u1, u1][FL] = (64*m2*q2*t1*u1)/sp^2
  
-BB[A, V, u1, u1][P] = -16*m2*t1 + 8*t1*u1 - (16*q2*t1*u1)/sp + 
+BB[A, V, u1, u1][gG] = 32*m2*t1 + 32*m2*u1 - 16*s*u1 - 16*t1*u1 + 
+     (16*q2*t1*u1)/sp - (16*t1^2*u1)/sp - (16*q2*u1^2)/sp + (16*u1^3)/sp
+ 
+BB[A, V, u1, u1][gL] = 0
+ 
+BB[A, V, u1, u1][x2g1] = -16*m2*t1 + 8*t1*u1 - (16*q2*t1*u1)/sp + 
      (16*t1^2*u1)/sp - (16*q2*t1*u1^2)/sp^2
  
-BB[V, A, t1, t1][GG] = -64*m2^2 - 32*m2*s - 64*m2*t1 - 32*m2*u1 - 16*t1*u1 + 
+BB[A, V, u1, u1][xF3] = -16*m2*t1 + (16*m2*q2*t1)/sp - (16*m2*t1^2)/sp - 
+     (16*m2*q2*u1)/sp - 8*t1*u1 + 4*n*t1*u1 + (16*m2*u1^2)/sp
+ 
+BB[V, A, t1, t1][FG] = -64*m2^2 - 32*m2*s - 64*m2*t1 - 32*m2*u1 - 16*t1*u1 + 
      8*n*t1*u1
  
-BB[V, A, t1, t1][L] = (64*m2*q2*t1*u1)/sp^2
+BB[V, A, t1, t1][FL] = (64*m2*q2*t1*u1)/sp^2
  
-BB[V, A, t1, t1][P] = -16*m2*u1 + 8*t1*u1 - (16*q2*t1*u1)/sp - 
+BB[V, A, t1, t1][gG] = 32*m2*t1 - 16*s*t1 - (16*q2*t1^2)/sp + (16*t1^3)/sp + 
+     32*m2*u1 - 16*t1*u1 + (16*q2*t1*u1)/sp - (16*t1*u1^2)/sp
+ 
+BB[V, A, t1, t1][gL] = 0
+ 
+BB[V, A, t1, t1][x2g1] = -16*m2*u1 + 8*t1*u1 - (16*q2*t1*u1)/sp - 
      (16*q2*t1^2*u1)/sp^2 + (16*t1*u1^2)/sp
  
-BB[V, A, t1, u1][GG] = -64*m2^2 + 16*s^2 - 16*m2*t1 + 16*s*t1 - 16*m2*u1 + 
+BB[V, A, t1, t1][xF3] = (-16*m2*q2*t1)/sp + (16*m2*t1^2)/sp - 16*m2*u1 + 
+     (16*m2*q2*u1)/sp - 8*t1*u1 + 4*n*t1*u1 - (16*m2*u1^2)/sp
+ 
+BB[V, A, t1, u1][FG] = -64*m2^2 + 16*s^2 - 16*m2*t1 + 16*s*t1 - 16*m2*u1 + 
      16*s*u1 - 32*t1*u1 + 8*n*t1*u1
  
-BB[V, A, t1, u1][L] = (64*m2*q2*t1*u1)/sp^2 - (32*q2*s*t1*u1)/sp^2
+BB[V, A, t1, u1][FL] = (64*m2*q2*t1*u1)/sp^2 - (32*q2*s*t1*u1)/sp^2
  
-BB[V, A, t1, u1][P] = -8*m2*t1 + 4*s*t1 + ((32*I)*epsi*q2*t1)/sp^2 - 
+BB[V, A, t1, u1][gG] = ((-128*I)*epsi*m2)/sp - ((64*I)*epsi*s)/sp + 8*s*t1 - 
+     ((32*I)*epsi*t1)/sp + (8*q2*t1^2)/sp - (8*t1^3)/sp - 8*s*u1 - 
+     ((32*I)*epsi*u1)/sp - (8*t1^2*u1)/sp - (8*q2*u1^2)/sp + (8*t1*u1^2)/sp + 
+     (8*u1^3)/sp
+ 
+BB[V, A, t1, u1][gL] = ((128*I)*epsi*q2*t1*u1)/sp^3
+ 
+BB[V, A, t1, u1][x2g1] = -8*m2*t1 + 4*s*t1 + ((32*I)*epsi*q2*t1)/sp^2 - 
      ((16*I)*epsi*t1)/sp - ((32*I)*epsi*t1^2)/sp^2 + (4*q2*t1^2)/sp - 
      (4*t1^3)/sp - 8*m2*u1 + 4*s*u1 - ((32*I)*epsi*q2*u1)/sp^2 + 
      ((16*I)*epsi*u1)/sp + (8*q2*t1*u1)/sp + (8*q2*t1^2*u1)/sp^2 - 
      (4*t1^2*u1)/sp + ((32*I)*epsi*u1^2)/sp^2 + (4*q2*u1^2)/sp + 
      (8*q2*t1*u1^2)/sp^2 - (4*t1*u1^2)/sp - (4*u1^3)/sp
  
-BB[V, A, u1, t1][GG] = -64*m2^2 + 16*s^2 - 16*m2*t1 + 16*s*t1 - 16*m2*u1 + 
+BB[V, A, t1, u1][xF3] = -4*s*t1 + ((16*I)*epsi*t1)/sp - (16*m2*q2*t1)/sp + 
+     (8*q2*s*t1)/sp + (16*m2*t1^2)/sp + (4*q2*t1^2)/sp - (8*s*t1^2)/sp - 
+     (4*t1^3)/sp - 16*m2*u1 + 4*s*u1 + ((16*I)*epsi*u1)/sp + 
+     (16*m2*q2*u1)/sp - (8*q2*s*u1)/sp + 16*t1*u1 - 4*n*t1*u1 - 
+     (4*t1^2*u1)/sp - (16*m2*u1^2)/sp - (4*q2*u1^2)/sp + (8*s*u1^2)/sp + 
+     (4*t1*u1^2)/sp + (4*u1^3)/sp
+ 
+BB[V, A, u1, t1][FG] = -64*m2^2 + 16*s^2 - 16*m2*t1 + 16*s*t1 - 16*m2*u1 + 
      16*s*u1 - 32*t1*u1 + 8*n*t1*u1
  
-BB[V, A, u1, t1][L] = (64*m2*q2*t1*u1)/sp^2 - (32*q2*s*t1*u1)/sp^2
+BB[V, A, u1, t1][FL] = (64*m2*q2*t1*u1)/sp^2 - (32*q2*s*t1*u1)/sp^2
  
-BB[V, A, u1, t1][P] = -8*m2*t1 + 4*s*t1 - ((32*I)*epsi*q2*t1)/sp^2 + 
+BB[V, A, u1, t1][gG] = ((128*I)*epsi*m2)/sp + ((64*I)*epsi*s)/sp + 8*s*t1 + 
+     ((32*I)*epsi*t1)/sp + (8*q2*t1^2)/sp - (8*t1^3)/sp - 8*s*u1 + 
+     ((32*I)*epsi*u1)/sp - (8*t1^2*u1)/sp - (8*q2*u1^2)/sp + (8*t1*u1^2)/sp + 
+     (8*u1^3)/sp
+ 
+BB[V, A, u1, t1][gL] = ((-128*I)*epsi*q2*t1*u1)/sp^3
+ 
+BB[V, A, u1, t1][x2g1] = -8*m2*t1 + 4*s*t1 - ((32*I)*epsi*q2*t1)/sp^2 + 
      ((16*I)*epsi*t1)/sp + ((32*I)*epsi*t1^2)/sp^2 + (4*q2*t1^2)/sp - 
      (4*t1^3)/sp - 8*m2*u1 + 4*s*u1 + ((32*I)*epsi*q2*u1)/sp^2 - 
      ((16*I)*epsi*u1)/sp + (8*q2*t1*u1)/sp + (8*q2*t1^2*u1)/sp^2 - 
      (4*t1^2*u1)/sp - ((32*I)*epsi*u1^2)/sp^2 + (4*q2*u1^2)/sp + 
      (8*q2*t1*u1^2)/sp^2 - (4*t1*u1^2)/sp - (4*u1^3)/sp
  
-BB[V, A, u1, u1][GG] = -64*m2^2 - 32*m2*s - 32*m2*t1 - 64*m2*u1 - 16*t1*u1 + 
+BB[V, A, u1, t1][xF3] = 16*m2*t1 - 4*s*t1 - ((16*I)*epsi*t1)/sp - 
+     (16*m2*q2*t1)/sp + (8*q2*s*t1)/sp + (16*m2*t1^2)/sp + (4*q2*t1^2)/sp - 
+     (8*s*t1^2)/sp - (4*t1^3)/sp + 4*s*u1 - ((16*I)*epsi*u1)/sp + 
+     (16*m2*q2*u1)/sp - (8*q2*s*u1)/sp - 16*t1*u1 + 4*n*t1*u1 - 
+     (4*t1^2*u1)/sp - (16*m2*u1^2)/sp - (4*q2*u1^2)/sp + (8*s*u1^2)/sp + 
+     (4*t1*u1^2)/sp + (4*u1^3)/sp
+ 
+BB[V, A, u1, u1][FG] = -64*m2^2 - 32*m2*s - 32*m2*t1 - 64*m2*u1 - 16*t1*u1 + 
      8*n*t1*u1
  
-BB[V, A, u1, u1][L] = (64*m2*q2*t1*u1)/sp^2
+BB[V, A, u1, u1][FL] = (64*m2*q2*t1*u1)/sp^2
  
-BB[V, A, u1, u1][P] = -16*m2*t1 + 8*t1*u1 - (16*q2*t1*u1)/sp + 
+BB[V, A, u1, u1][gG] = -32*m2*t1 - 32*m2*u1 + 16*s*u1 + 16*t1*u1 - 
+     (16*q2*t1*u1)/sp + (16*t1^2*u1)/sp + (16*q2*u1^2)/sp - (16*u1^3)/sp
+ 
+BB[V, A, u1, u1][gL] = 0
+ 
+BB[V, A, u1, u1][x2g1] = -16*m2*t1 + 8*t1*u1 - (16*q2*t1*u1)/sp + 
      (16*t1^2*u1)/sp - (16*q2*t1*u1^2)/sp^2
  
-BB[V, V, t1, t1][GG] = -32*m2^2 + 16*m2*s - 8*m2*n*s - 8*m2*n*t1 + 16*m2*u1 - 
+BB[V, A, u1, u1][xF3] = 16*m2*t1 - (16*m2*q2*t1)/sp + (16*m2*t1^2)/sp + 
+     (16*m2*q2*u1)/sp + 8*t1*u1 - 4*n*t1*u1 - (16*m2*u1^2)/sp
+ 
+BB[V, V, t1, t1][FG] = -32*m2^2 + 16*m2*s - 8*m2*n*s - 8*m2*n*t1 + 16*m2*u1 - 
      8*m2*n*u1 + 8*t1*u1 - 8*n*t1*u1 + 2*n^2*t1*u1
  
-BB[V, V, t1, t1][L] = (32*m2*q2*t1*u1)/sp^2
+BB[V, V, t1, t1][FL] = (32*m2*q2*t1*u1)/sp^2
  
-BB[V, V, t1, t1][P] = -8*m2*u1 + 4*t1*u1 - (8*q2*t1*u1)/sp - 
+BB[V, V, t1, t1][gG] = 0
+ 
+BB[V, V, t1, t1][gL] = 0
+ 
+BB[V, V, t1, t1][x2g1] = -8*m2*u1 + 4*t1*u1 - (8*q2*t1*u1)/sp - 
      (8*q2*t1^2*u1)/sp^2 + (8*t1*u1^2)/sp
  
-BB[V, V, t1, u1][GG] = -32*m2^2 + 32*m2*s - 8*m2*n*s - 8*s^2 + 4*n*s^2 + 
+BB[V, V, t1, t1][xF3] = 0
+ 
+BB[V, V, t1, u1][FG] = -32*m2^2 + 32*m2*s - 8*m2*n*s - 8*s^2 + 4*n*s^2 + 
      24*m2*t1 - 8*m2*n*t1 - 8*s*t1 + 4*n*s*t1 + 24*m2*u1 - 8*m2*n*u1 - 
      8*s*u1 + 4*n*s*u1 + 16*t1*u1 - 12*n*t1*u1 + 2*n^2*t1*u1
  
-BB[V, V, t1, u1][L] = (32*m2*q2*t1*u1)/sp^2 - (16*q2*s*t1*u1)/sp^2
+BB[V, V, t1, u1][FL] = (32*m2*q2*t1*u1)/sp^2 - (16*q2*s*t1*u1)/sp^2
  
-BB[V, V, t1, u1][P] = -4*m2*t1 + 2*s*t1 + (2*q2*t1^2)/sp - (2*t1^3)/sp - 
+BB[V, V, t1, u1][gG] = ((-64*I)*epsi*m2)/sp + ((32*I)*epsi*s)/sp - 
+     ((16*I)*epsi*n*s)/sp + ((16*I)*epsi*t1)/sp - ((8*I)*epsi*n*t1)/sp + 
+     ((16*I)*epsi*u1)/sp - ((8*I)*epsi*n*u1)/sp
+ 
+BB[V, V, t1, u1][gL] = ((64*I)*epsi*q2*t1*u1)/sp^3
+ 
+BB[V, V, t1, u1][x2g1] = -4*m2*t1 + 2*s*t1 + (2*q2*t1^2)/sp - (2*t1^3)/sp - 
      4*m2*u1 + 2*s*u1 + (4*q2*t1*u1)/sp + (4*q2*t1^2*u1)/sp^2 - 
      (2*t1^2*u1)/sp + (2*q2*u1^2)/sp + (4*q2*t1*u1^2)/sp^2 - (2*t1*u1^2)/sp - 
      (2*u1^3)/sp
  
-BB[V, V, u1, t1][GG] = -32*m2^2 + 32*m2*s - 8*m2*n*s - 8*s^2 + 4*n*s^2 + 
+BB[V, V, t1, u1][xF3] = ((8*I)*epsi*t1)/sp + ((8*I)*epsi*u1)/sp
+ 
+BB[V, V, u1, t1][FG] = -32*m2^2 + 32*m2*s - 8*m2*n*s - 8*s^2 + 4*n*s^2 + 
      24*m2*t1 - 8*m2*n*t1 - 8*s*t1 + 4*n*s*t1 + 24*m2*u1 - 8*m2*n*u1 - 
      8*s*u1 + 4*n*s*u1 + 16*t1*u1 - 12*n*t1*u1 + 2*n^2*t1*u1
  
-BB[V, V, u1, t1][L] = (32*m2*q2*t1*u1)/sp^2 - (16*q2*s*t1*u1)/sp^2
+BB[V, V, u1, t1][FL] = (32*m2*q2*t1*u1)/sp^2 - (16*q2*s*t1*u1)/sp^2
  
-BB[V, V, u1, t1][P] = -4*m2*t1 + 2*s*t1 + (2*q2*t1^2)/sp - (2*t1^3)/sp - 
+BB[V, V, u1, t1][gG] = ((64*I)*epsi*m2)/sp - ((32*I)*epsi*s)/sp + 
+     ((16*I)*epsi*n*s)/sp - ((16*I)*epsi*t1)/sp + ((8*I)*epsi*n*t1)/sp - 
+     ((16*I)*epsi*u1)/sp + ((8*I)*epsi*n*u1)/sp
+ 
+BB[V, V, u1, t1][gL] = ((-64*I)*epsi*q2*t1*u1)/sp^3
+ 
+BB[V, V, u1, t1][x2g1] = -4*m2*t1 + 2*s*t1 + (2*q2*t1^2)/sp - (2*t1^3)/sp - 
      4*m2*u1 + 2*s*u1 + (4*q2*t1*u1)/sp + (4*q2*t1^2*u1)/sp^2 - 
      (2*t1^2*u1)/sp + (2*q2*u1^2)/sp + (4*q2*t1*u1^2)/sp^2 - (2*t1*u1^2)/sp - 
      (2*u1^3)/sp
  
-BB[V, V, u1, u1][GG] = -32*m2^2 + 16*m2*s - 8*m2*n*s + 16*m2*t1 - 8*m2*n*t1 - 
+BB[V, V, u1, t1][xF3] = ((-8*I)*epsi*t1)/sp - ((8*I)*epsi*u1)/sp
+ 
+BB[V, V, u1, u1][FG] = -32*m2^2 + 16*m2*s - 8*m2*n*s + 16*m2*t1 - 8*m2*n*t1 - 
      8*m2*n*u1 + 8*t1*u1 - 8*n*t1*u1 + 2*n^2*t1*u1
  
-BB[V, V, u1, u1][L] = (32*m2*q2*t1*u1)/sp^2
+BB[V, V, u1, u1][FL] = (32*m2*q2*t1*u1)/sp^2
  
-BB[V, V, u1, u1][P] = -8*m2*t1 + 4*t1*u1 - (8*q2*t1*u1)/sp + (8*t1^2*u1)/sp - 
-     (8*q2*t1*u1^2)/sp^2
+BB[V, V, u1, u1][gG] = 0
+ 
+BB[V, V, u1, u1][gL] = 0
+ 
+BB[V, V, u1, u1][x2g1] = -8*m2*t1 + 4*t1*u1 - (8*q2*t1*u1)/sp + 
+     (8*t1^2*u1)/sp - (8*q2*t1*u1^2)/sp^2
+ 
+BB[V, V, u1, u1][xF3] = 0
