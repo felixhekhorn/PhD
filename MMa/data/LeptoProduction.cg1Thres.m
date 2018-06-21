43596539e991e1f0d9692fef69055d80ca2f2416
Felix Hekhorn <felix.hekhorn@student.uni-tuebingen.de>
2018-06-21 18:33:20 +0200

    update data2+img2; work on thresh

---
+++ MMa/data/LeptoProduction.cg1Thres.m
@@ -0,0 +1,285 @@
+(* Created by Wolfram Mathematica 8.0 : www.wolfram.com *)
+{{V, V, F2, OK} -> (Pi*\[Rho]q)/(32 - 32*\[Rho]q) + 
+   \[Beta]*((\[Rho]q*(300*(-1 + \[Rho]q)^2 + 
+        Pi^2*(-7 + (21 - 8*\[Rho]q)*\[Rho]q)))/(192*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*
+       Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*
+       Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-1 + \[Rho]q*(6 + \[Rho]q))*Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (15*\[Rho]q*ln[2])/(8*Pi - 8*Pi*\[Rho]q) + 
+     (9*\[Rho]q*ln[2]^2)/(8*Pi*(-1 + \[Rho]q)) + 
+     ((5*\[Rho]q)/(4*Pi - 4*Pi*\[Rho]q) + (3*\[Rho]q*ln[2])/
+        (2*Pi*(-1 + \[Rho]q)))*ln[\[Beta]] + (\[Rho]q*ln[\[Beta]]^2)/
+      (2*Pi*(-1 + \[Rho]q)) - (3*\[Rho]q*ln[\[Chi]q])/
+      (16*Pi*\[Beta]q*(1 - \[Rho]q)) + 
+     (\[Rho]q*(3 + \[Rho]q*(-1 + 4*\[Rho]q))*ln[\[Chi]q]^2)/
+      (64*Pi*(-1 + \[Rho]q)^3) + ((-3 + \[Rho]q)*\[Rho]q*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/(8*Pi*(-2 + \[Rho]q)*
+       (-1 + \[Rho]q)) + (\[Rho]q*ln[(1 + \[Chi]q)^2/(2*\[Chi]q)]^2)/
+      (8*Pi - 8*Pi*\[Rho]q)), {V, V, F2, QED} -> 
+  (Pi*\[Rho]q)/(32*(-1 + \[Rho]q)) + 
+   \[Beta]*((\[Rho]q*(-12*(-1 + \[Rho]q)^2*(-9 + 5*\[Rho]q) - 
+        Pi^2*(-2 + \[Rho]q)*(3 + \[Rho]q + 2*\[Rho]q^2)))/
+      (192*Pi*(-2 + \[Rho]q)*(-1 + \[Rho]q)^3) + 
+     ((\[Rho]q - 2*\[Rho]q^2 - 5*\[Rho]q^3)*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 2*\[Rho]q^2 - 5*\[Rho]q^3)*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-1 + \[Rho]q*(2 + 5*\[Rho]q))*
+       Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-1 + \[Rho]q*(2 + 5*\[Rho]q))*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     ((\[Rho]q - 2*\[Rho]q^2 - 5*\[Rho]q^3)*
+       Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 + 5*\[Rho]q^2)*Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (3*\[Rho]q*ln[\[Chi]q])/
+      (16*Pi*\[Beta]q*(1 - \[Rho]q)) - (\[Rho]q*(1 + \[Rho]q + 4*\[Rho]q^2)*
+       ln[\[Chi]q]^2)/(64*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-3 + 2*\[Rho]q)*ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/
+      (8*Pi*(-2 + \[Rho]q)^2*(-1 + \[Rho]q))), 
+ {A, A, F2, OK} -> (Pi*\[Rho]q*(-1 + 2*\[Rho]q))/(32*(-1 + \[Rho]q)) + 
+   \[Beta]*((\[Rho]q*(-300*(-1 + \[Rho]q)^2*(-1 + 2*\[Rho]q) + 
+        Pi^2*(-7 + \[Rho]q*(32 + \[Rho]q*(-33 + 10*\[Rho]q)))))/
+      (192*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 3*\[Rho]q^2)*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 3*\[Rho]q^2)*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-1 + 3*\[Rho]q)*Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q*(-1 + 3*\[Rho]q)*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     ((\[Rho]q - 3*\[Rho]q^2)*Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 4*\[Rho]q^2 - 2*\[Rho]q^3)*
+       Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^2) + 
+     (15*\[Rho]q*(-1 + 2*\[Rho]q)*ln[2])/(8*Pi*(-1 + \[Rho]q)) + 
+     (9*(1 - 2*\[Rho]q)*\[Rho]q*ln[2]^2)/(8*Pi*(-1 + \[Rho]q)) + 
+     ((5*\[Rho]q*(-1 + 2*\[Rho]q))/(4*Pi*(-1 + \[Rho]q)) + 
+       (3*(1 - 2*\[Rho]q)*\[Rho]q*ln[2])/(2*Pi*(-1 + \[Rho]q)))*ln[\[Beta]] - 
+     ((\[Rho]q - 2*\[Rho]q^2)*ln[\[Beta]]^2)/(2*Pi - 2*Pi*\[Rho]q) - 
+     (\[Rho]q*(1 + \[Rho]q)*ln[\[Chi]q])/(16*Pi*(1 - \[Rho]q)^(5/2)) + 
+     (\[Rho]q*(-3 + (7 - 8*\[Rho]q)*\[Rho]q)*ln[\[Chi]q]^2)/
+      (64*Pi*(-1 + \[Rho]q)^2) + ((\[Rho]q + \[Rho]q^3 - \[Rho]q^4)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/(8*Pi*(-2 + \[Rho]q)*
+       (-1 + \[Rho]q)^2) + ((\[Rho]q - 2*\[Rho]q^2)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)]^2)/(8*Pi - 8*Pi*\[Rho]q)), 
+ {A, A, F2, QED} -> (Pi*(1 - 2*\[Rho]q)*\[Rho]q)/(32*(-1 + \[Rho]q)) + 
+   \[Beta]*((\[Rho]q*(12*(-1 + \[Rho]q)^2*(-5 + 2*\[Rho]q)*(-1 + 2*\[Rho]q) + 
+        Pi^2*(-2 + \[Rho]q)*(-3 + (-2 + \[Rho]q)*\[Rho]q*(-5 + 6*\[Rho]q))))/
+      (192*Pi*(-2 + \[Rho]q)*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 - 4*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^2) + 
+     (\[Rho]q*(1 + \[Rho]q*(-1 + 4*\[Rho]q))*ln[\[Chi]q])/
+      (16*Pi*(1 - \[Rho]q)^(5/2)) + (\[Rho]q*(1 + \[Rho]q*(-3 + 4*\[Rho]q))*
+       ln[\[Chi]q]^2)/(64*Pi*(-1 + \[Rho]q)^2) + 
+     (\[Rho]q*(-1 + 6*\[Rho]q - 5*\[Rho]q^3 + 2*\[Rho]q^4)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/(8*Pi*(2 - 3*\[Rho]q + \[Rho]q^2)^
+        2)), {V, V, FL, OK} -> 
+  (Pi*\[Beta]^2*\[Rho]q^2)/(12*(-1 + \[Rho]q)^3) + 
+   \[Beta]*((Pi*\[Rho]q^2)/(48*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q^2*Li2[1 - \[Chi]q])/(8*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q^2*Li2[(-1 + \[Chi]q)/\[Chi]q])/(8*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q^2*Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(8*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q^2*Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/
+      (8*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q^2*Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/
+      (8*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q^2*Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/
+      (8*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q^2*ln[\[Chi]q]^2)/
+      (16*Pi*(-1 + \[Rho]q)^3)) + 
+   \[Beta]^3*((\[Rho]q^2*(-2192*(-1 + \[Rho]q)^2 + 
+        3*Pi^2*(16 + \[Rho]q*(-7 + 3*\[Rho]q))))/(432*Pi*(-1 + \[Rho]q)^5) - 
+     (\[Rho]q^3*(7 + 5*\[Rho]q)*Li2[1 - \[Chi]q])/(24*Pi*(-1 + \[Rho]q)^5) - 
+     (\[Rho]q^3*(7 + 5*\[Rho]q)*Li2[(-1 + \[Chi]q)/\[Chi]q])/
+      (24*Pi*(-1 + \[Rho]q)^5) + (\[Rho]q^3*(7 + 5*\[Rho]q)*
+       Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(24*Pi*(-1 + \[Rho]q)^5) + 
+     (\[Rho]q^3*(7 + 5*\[Rho]q)*Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/
+      (24*Pi*(-1 + \[Rho]q)^5) - (\[Rho]q^3*(7 + 5*\[Rho]q)*
+       Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/(24*Pi*(-1 + \[Rho]q)^5) - 
+     (\[Rho]q^2*(4 + 5*\[Rho]q)*Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/
+      (24*Pi*(-1 + \[Rho]q)^4) - (5*\[Rho]q^2*ln[2]^2)/
+      (3*Pi*(-1 + \[Rho]q)^3) + 
+     ln[2]*((19*\[Rho]q^2)/(3*Pi*(-1 + \[Rho]q)^3) - (2*\[Rho]q^2*ln[64])/
+        (9*Pi*(-1 + \[Rho]q)^3)) + ((38*\[Rho]q^2)/(9*Pi*(-1 + \[Rho]q)^3) - 
+       (4*\[Rho]q^2*ln[2])/(Pi*(-1 + \[Rho]q)^3))*ln[\[Beta]] - 
+     (4*\[Rho]q^2*ln[\[Beta]]^2)/(3*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q^2*(-3 + Sqrt[1 - \[Rho]q] - (9 + Sqrt[1 - \[Rho]q])*\[Rho]q)*
+       ln[\[Chi]q])/(12*Pi*(1 - \[Rho]q)^(9/2)) + 
+     ((4 - 13*\[Rho]q)*\[Rho]q^2*ln[\[Chi]q]^2)/(48*Pi*(-1 + \[Rho]q)^4) + 
+     ((1 + Sqrt[1 - \[Rho]q])*\[Rho]q^2*ln[2/(1 + \[Chi]q)^2])/
+      (12*Pi*(1 - \[Rho]q)^(7/2)) + 
+     (\[Rho]q^2*(-11 + 4*Sqrt[1 - \[Rho]q] + 
+        \[Rho]q*(-4*(3 + Sqrt[1 - \[Rho]q]) + (21 + Sqrt[1 - \[Rho]q] - 
+            6*\[Rho]q)*\[Rho]q))*ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/
+      (12*Pi*(-2 + \[Rho]q)^2*(-1 + \[Rho]q)^4) + 
+     (\[Rho]q^2*ln[(1 + \[Chi]q)^2/(2*\[Chi]q)]^2)/(3*Pi*(-1 + \[Rho]q)^3) + 
+     ((\[Rho]q^4 + 4*\[Rho]q^5 - 2*\[Rho]q^6)*
+       ln[(2*\[Chi]q)/(1 + \[Chi]q^2)])/(48*Pi*(-2 + \[Rho]q)^2*
+       (-1 + \[Rho]q)^5) + (\[Rho]q^2*(-7 + 2*(12 - 7*\[Rho]q)*\[Rho]q)*
+       ln[(1 + \[Chi]q^2)/(2*\[Chi]q)])/(48*Pi*(-1 + \[Rho]q)^5) - 
+     (\[Rho]q^2*(-7 + 3*\[Rho]q)*(1 + (-3 + \[Rho]q)*\[Rho]q)*
+       ln[1 + (2*\[Chi]q)/(1 + \[Chi]q^2)])/(12*Pi*(-2 + \[Rho]q)^2*
+       (-1 + \[Rho]q)^4)), {V, V, FL, QED} -> 
+  -(Pi*\[Beta]^2*\[Rho]q^2)/(12*(-1 + \[Rho]q)^3) + 
+   \[Beta]^3*((\[Rho]q^2*(12*(-1 + \[Rho]q)^2*(-3 + 2*\[Rho]q) + 
+        Pi^2*(-2 + \[Rho]q)*(3 + (-14 + \[Rho]q)*\[Rho]q)))/
+      (72*Pi*(-2 + \[Rho]q)*(-1 + \[Rho]q)^5) + 
+     (\[Rho]q^2*(-1 + \[Rho]q*(10 + \[Rho]q))*Li2[1 - \[Chi]q])/
+      (12*Pi*(-1 + \[Rho]q)^5) + (\[Rho]q^2*(-1 + \[Rho]q*(10 + \[Rho]q))*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(12*Pi*(-1 + \[Rho]q)^5) - 
+     (\[Rho]q^2*(-1 + \[Rho]q*(10 + \[Rho]q))*
+       Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(12*Pi*(-1 + \[Rho]q)^5) + 
+     (\[Rho]q^2*(-1 + \[Rho]q*(10 + \[Rho]q))*
+       Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/(12*Pi*(-1 + \[Rho]q)^5) + 
+     (\[Rho]q^2*Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/(12*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q^2*(-1 + \[Rho]q*(10 + \[Rho]q))*
+       Li2[1 - (2*\[Chi]q)/(1 + \[Chi]q^2)])/(12*Pi*(-1 + \[Rho]q)^5) + 
+     (\[Rho]q^2*(1 - Sqrt[1 - \[Rho]q] + (21 + Sqrt[1 - \[Rho]q] - 2*\[Rho]q)*
+         \[Rho]q)*ln[\[Chi]q])/(12*Pi*(1 - \[Rho]q)^(9/2)) + 
+     (\[Rho]q^2*ln[\[Chi]q]^2)/(24*Pi*(-1 + \[Rho]q)^3) - 
+     ((1 + Sqrt[1 - \[Rho]q])*\[Rho]q^2*ln[2/(1 + \[Chi]q)^2])/
+      (12*Pi*(1 - \[Rho]q)^(7/2)) + 
+     (\[Rho]q^2*(-Sqrt[1 - \[Rho]q] + 
+        (14 + \[Rho]q*(37 + \[Rho]q*(-39 + 8*\[Rho]q)))/(-2 + \[Rho]q)^2)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/(12*Pi*(-1 + \[Rho]q)^4) + 
+     (\[Rho]q^3*ln[(2*\[Chi]q)/(1 + \[Chi]q^2)])/(24*Pi*(-2 + \[Rho]q)*
+       (-1 + \[Rho]q)^4) + (\[Rho]q^2*(-3 + 2*\[Rho]q)*
+       ln[(1 + \[Chi]q^2)/(2*\[Chi]q)])/(24*Pi*(-1 + \[Rho]q)^4) + 
+     ((-3 + \[Rho]q)*\[Rho]q^2*ln[1 + (2*\[Chi]q)/(1 + \[Chi]q^2)])/
+      (12*Pi*(-2 + \[Rho]q)*(-1 + \[Rho]q)^3)), 
+ {A, A, FL, OK} -> (Pi*\[Rho]q*(-1 + 2*\[Rho]q))/(32*(-1 + \[Rho]q)) + 
+   \[Beta]*((\[Rho]q*(-60*(-1 + \[Rho]q)^2*(-1 + 10*\[Rho]q) + 
+        Pi^2*(1 + \[Rho]q*(16 + 5*\[Rho]q*(-5 + 2*\[Rho]q)))))/
+      (192*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 3*\[Rho]q^2)*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 3*\[Rho]q^2)*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-1 + 3*\[Rho]q)*Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q*(-1 + 3*\[Rho]q)*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     ((\[Rho]q - 3*\[Rho]q^2)*Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 4*\[Rho]q^2 - 2*\[Rho]q^3)*
+       Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^2) + 
+     (15*\[Rho]q^2*ln[2])/(4*Pi*(-1 + \[Rho]q)) + (9*\[Rho]q^2*ln[2]^2)/
+      (4*Pi - 4*Pi*\[Rho]q) + ((5*\[Rho]q^2)/(2*Pi*(-1 + \[Rho]q)) + 
+       (3*\[Rho]q^2*ln[2])/(Pi - Pi*\[Rho]q))*ln[\[Beta]] + 
+     (\[Rho]q^2*ln[\[Beta]]^2)/(Pi - Pi*\[Rho]q) - 
+     (\[Rho]q*(1 + \[Rho]q)*ln[\[Chi]q])/(16*Pi*(1 - \[Rho]q)^(5/2)) + 
+     (\[Rho]q*(-3 + (7 - 8*\[Rho]q)*\[Rho]q)*ln[\[Chi]q]^2)/
+      (64*Pi*(-1 + \[Rho]q)^2) + ((\[Rho]q + \[Rho]q^3 - \[Rho]q^4)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/(8*Pi*(-2 + \[Rho]q)*
+       (-1 + \[Rho]q)^2) + ((\[Rho]q - 2*\[Rho]q^2)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)]^2)/(8*Pi - 8*Pi*\[Rho]q)), 
+ {A, A, FL, QED} -> (Pi*(1 - 2*\[Rho]q)*\[Rho]q)/(32*(-1 + \[Rho]q)) + 
+   \[Beta]*((\[Rho]q*(12*(-1 + \[Rho]q)^2*(-5 + 2*\[Rho]q)*(-1 + 2*\[Rho]q) + 
+        Pi^2*(-2 + \[Rho]q)*(-3 + (-2 + \[Rho]q)*\[Rho]q*(-5 + 6*\[Rho]q))))/
+      (192*Pi*(-2 + \[Rho]q)*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 - 4*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^2) + 
+     (\[Rho]q*(1 + \[Rho]q*(-1 + 4*\[Rho]q))*ln[\[Chi]q])/
+      (16*Pi*(1 - \[Rho]q)^(5/2)) + (\[Rho]q*(1 + \[Rho]q*(-3 + 4*\[Rho]q))*
+       ln[\[Chi]q]^2)/(64*Pi*(-1 + \[Rho]q)^2) + 
+     (\[Rho]q*(-1 + 6*\[Rho]q - 5*\[Rho]q^3 + 2*\[Rho]q^4)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/(8*Pi*(2 - 3*\[Rho]q + \[Rho]q^2)^
+        2)), {V, V, x2g1, OK} -> (Pi*\[Rho]q)/(32 - 32*\[Rho]q) + 
+   \[Beta]*((\[Rho]q*(300*(-1 + \[Rho]q)^2 + 
+        Pi^2*(-7 + (21 - 8*\[Rho]q)*\[Rho]q)))/(192*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*
+       Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 + \[Rho]q*(2 + 3*\[Rho]q))*
+       Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-1 + \[Rho]q*(6 + \[Rho]q))*Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (15*\[Rho]q*ln[2])/(8*Pi - 8*Pi*\[Rho]q) + 
+     (9*\[Rho]q*ln[2]^2)/(8*Pi*(-1 + \[Rho]q)) + 
+     ((5*\[Rho]q)/(4*Pi - 4*Pi*\[Rho]q) + (3*\[Rho]q*ln[2])/
+        (2*Pi*(-1 + \[Rho]q)))*ln[\[Beta]] + (\[Rho]q*ln[\[Beta]]^2)/
+      (2*Pi*(-1 + \[Rho]q)) - (3*\[Rho]q*ln[\[Chi]q])/
+      (16*Pi*\[Beta]q*(1 - \[Rho]q)) + 
+     (\[Rho]q*(3 + \[Rho]q*(-1 + 4*\[Rho]q))*ln[\[Chi]q]^2)/
+      (64*Pi*(-1 + \[Rho]q)^3) + ((-3 + \[Rho]q)*\[Rho]q*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/(8*Pi*(-2 + \[Rho]q)*
+       (-1 + \[Rho]q)) + (\[Rho]q*ln[(1 + \[Chi]q)^2/(2*\[Chi]q)]^2)/
+      (8*Pi - 8*Pi*\[Rho]q)), {V, V, x2g1, QED} -> 
+  (Pi*\[Rho]q)/(32*(-1 + \[Rho]q)) + 
+   \[Beta]*((\[Rho]q*(-12*(-1 + \[Rho]q)^2*(-9 + 5*\[Rho]q) - 
+        Pi^2*(-2 + \[Rho]q)*(3 + \[Rho]q + 2*\[Rho]q^2)))/
+      (192*Pi*(-2 + \[Rho]q)*(-1 + \[Rho]q)^3) + 
+     ((\[Rho]q - 2*\[Rho]q^2 - 5*\[Rho]q^3)*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 2*\[Rho]q^2 - 5*\[Rho]q^3)*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-1 + \[Rho]q*(2 + 5*\[Rho]q))*
+       Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-1 + \[Rho]q*(2 + 5*\[Rho]q))*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     ((\[Rho]q - 2*\[Rho]q^2 - 5*\[Rho]q^3)*
+       Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 + 5*\[Rho]q^2)*Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (3*\[Rho]q*ln[\[Chi]q])/
+      (16*Pi*\[Beta]q*(1 - \[Rho]q)) - (\[Rho]q*(1 + \[Rho]q + 4*\[Rho]q^2)*
+       ln[\[Chi]q]^2)/(64*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-3 + 2*\[Rho]q)*ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/
+      (8*Pi*(-2 + \[Rho]q)^2*(-1 + \[Rho]q))), 
+ {A, A, x2g1, OK} -> (Pi*\[Rho]q*(-1 + 2*\[Rho]q))/(32*(-1 + \[Rho]q)) + 
+   \[Beta]*(-(\[Rho]q*(60*(-1 + \[Rho]q)^2*(-5 + 2*\[Rho]q) + 
+         Pi^2*(7 + \[Rho]q*(-16 + \[Rho]q + 6*\[Rho]q^2))))/
+      (192*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 3*\[Rho]q^2)*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 3*\[Rho]q^2)*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(-1 + 3*\[Rho]q)*Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q*(-1 + 3*\[Rho]q)*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     ((\[Rho]q - 3*\[Rho]q^2)*Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/
+      (32*Pi*(-1 + \[Rho]q)^3) + ((\[Rho]q - 4*\[Rho]q^2 - 2*\[Rho]q^3)*
+       Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^2) + 
+     (15*\[Rho]q*ln[2])/(8*Pi - 8*Pi*\[Rho]q) + (9*\[Rho]q*ln[2]^2)/
+      (8*Pi*(-1 + \[Rho]q)) + ((5*\[Rho]q)/(4*Pi - 4*Pi*\[Rho]q) + 
+       (3*\[Rho]q*ln[2])/(2*Pi*(-1 + \[Rho]q)))*ln[\[Beta]] + 
+     (\[Rho]q*ln[\[Beta]]^2)/(2*Pi*(-1 + \[Rho]q)) - 
+     (\[Rho]q*(1 + \[Rho]q)*ln[\[Chi]q])/(16*Pi*(1 - \[Rho]q)^(5/2)) + 
+     (\[Rho]q*(-3 + (7 - 8*\[Rho]q)*\[Rho]q)*ln[\[Chi]q]^2)/
+      (64*Pi*(-1 + \[Rho]q)^2) + ((\[Rho]q + \[Rho]q^3 - \[Rho]q^4)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/(8*Pi*(-2 + \[Rho]q)*
+       (-1 + \[Rho]q)^2) + ((\[Rho]q - 2*\[Rho]q^2)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)]^2)/(8*Pi - 8*Pi*\[Rho]q)), 
+ {A, A, x2g1, QED} -> (Pi*(1 - 2*\[Rho]q)*\[Rho]q)/(32*(-1 + \[Rho]q)) + 
+   \[Beta]*((\[Rho]q*(12*(-1 + \[Rho]q)^2*(-5 + 2*\[Rho]q)*(-1 + 2*\[Rho]q) + 
+        Pi^2*(-2 + \[Rho]q)*(-3 + (-2 + \[Rho]q)*\[Rho]q*(-5 + 6*\[Rho]q))))/
+      (192*Pi*(-2 + \[Rho]q)*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*Li2[1 - \[Chi]q])/
+      (32*Pi*(-1 + \[Rho]q)^3) + (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[(-1 + \[Chi]q)/\[Chi]q])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[-(-1 + \[Chi]q)^2/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^3) - 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[(-1 + \[Chi]q)^2/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 - 3*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[(-2*\[Chi]q)/(1 + \[Chi]q^2)])/(32*Pi*(-1 + \[Rho]q)^3) + 
+     (\[Rho]q*(1 - 4*\[Rho]q + 6*\[Rho]q^2)*
+       Li2[-(1 + \[Chi]q^2)/(2*\[Chi]q)])/(32*Pi*(-1 + \[Rho]q)^2) + 
+     (\[Rho]q*(1 + \[Rho]q*(-1 + 4*\[Rho]q))*ln[\[Chi]q])/
+      (16*Pi*(1 - \[Rho]q)^(5/2)) + (\[Rho]q*(1 + \[Rho]q*(-3 + 4*\[Rho]q))*
+       ln[\[Chi]q]^2)/(64*Pi*(-1 + \[Rho]q)^2) + 
+     (\[Rho]q*(-1 + 6*\[Rho]q - 5*\[Rho]q^3 + 2*\[Rho]q^4)*
+       ln[(1 + \[Chi]q)^2/(2*\[Chi]q)])/(8*Pi*(2 - 3*\[Rho]q + \[Rho]q^2)^2))}
