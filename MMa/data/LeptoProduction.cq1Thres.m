875f8ee8f93c8e6b43c76fc79bde341ca4ef0baf
Felix Hekhorn <felix.hekhorn@student.uni-tuebingen.de>
2018-06-25 17:58:16 +0200

    work on Lepto/ana, Lepto/thres, tex/3

---
+++ MMa/data/LeptoProduction.cq1Thres.m
@@ -0,0 +1,13 @@
+(* Created by Wolfram Mathematica 8.0 : www.wolfram.com *)
+{{V, V, F2} -> (\[Beta]^3*\[Rho]q^2*(-13 + 18*ln[2] + 12*ln[\[Beta]]))/
+   (54*Pi*(-1 + \[Rho]q)^2), {A, A, F2} -> 
+  -(\[Beta]^3*\[Rho]q^2*(-1 + 2*\[Rho]q)*(-13 + 18*ln[2] + 12*ln[\[Beta]]))/
+   (54*Pi*(-1 + \[Rho]q)^2), {V, V, FL} -> 
+  (-4*\[Beta]^5*\[Rho]q^3*(-77 + 90*ln[2] + 60*ln[\[Beta]]))/
+   (675*Pi*(-1 + \[Rho]q)^4), {A, A, FL} -> 
+  -(\[Beta]^3*\[Rho]q^3*(-26 + 36*ln[2] + 24*ln[\[Beta]]))/
+   (54*Pi*(-1 + \[Rho]q)^2), {V, V, x2g1} -> 
+  (\[Beta]^3*\[Rho]q^2*(-8 + 9*ln[2] + 6*ln[\[Beta]]))/
+   (27*Pi*(-1 + \[Rho]q)^2), {A, A, x2g1} -> 
+  (\[Beta]^3*\[Rho]q^2*(-13 + 18*ln[2] + 12*ln[\[Beta]]))/
+   (54*Pi*(-1 + \[Rho]q)^2)}
