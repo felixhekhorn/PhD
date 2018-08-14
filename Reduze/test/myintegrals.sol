030abbecb2880b100f262318c555c52a9a56fa69
Felix Hekhorn <felix.hekhorn@student.uni-tuebingen.de>
2018-08-14 18:28:01 +0200

    work on Reduze

---
+++ Reduze/test/myintegrals.sol
@@ -0,0 +1,12 @@
+  planarbox 3 11  3 0  1 1 0 1
+   1
+    planarbox 3 7  3 0  1 1 1 0
+     -1
+;
+  planarbox 4 15  4 0  1 1 1 1
+   1
+    planarbox 3 7  3 0  1 1 1 0
+     1/2*m2^(-1)*sp^(-1)*(4*q2-d*sp+4*sp-q2*d)
+    planarbox 2 3  2 0  1 1 0 0
+     m2^(-1)*sp^(-2)*(3*q2-d*sp+3*sp-q2*d)
+;
